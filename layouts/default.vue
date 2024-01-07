e9944c806e4745fa36598e7619850227ba64707f
aronm99 <markanda@mcmaster.ca>
2024-01-06 20:59:00 -0500

    update navigation panel

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,63 +1,98 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <div class="d-flex flex-column align-center pa-2">
-        <v-switch
-          class="mt-auto"
-          v-model="themeVal"
-          true-value="Light"
-          false-value="Dark"
-          true-icon="mdi-weather-night"
-          false-icon="mdi-weather-sunny"
-          :color="`${themeVal ? 'grey-lighten-5' : 'grey-darken-4'}`"
-          inset
-          @update:model-value="toggleTheme"
+    <v-navigation-drawer
+      :rail="rail"
+    >
+      <v-list class="d-flex flex-column h-screen">
+        <v-list-item
+          :prepend-icon="rail ? 'mdi-chevron-right' : 'mdi-chevron-left'"
+          @click="toggleRail"
         />
-        <!-- Menu Trigger Button -->
-        <v-btn @click="toggleMenu" prepend-icon="" size="large">
-          <template v-slot:prepend>
-            <v-avatar>
-              <v-img v-if="user.profilePicture" :src="user.profilePicture" alt="User Profile" />
-            </v-avatar>
-          </template>
-          {{ user.name }}
-        </v-btn>
 
-        <!-- Menu -->
-        <v-menu v-model="menu" :position-x="position.x" :position-y="position.y">
-          <v-list>
-            <v-list-item @click="menuItemClicked('Profile')">
-              <v-list-item-icon>
-                <v-icon>mdi-account</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>Profile</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item @click="menuItemClicked('Settings')">
-              <v-list-item-icon>
-                <v-icon>mdi-cog</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>Settings</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item @click="menuItemClicked('Logout')">
-              <v-list-item-icon>
-                <v-icon>mdi-logout</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title>Logout</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </div>
+        <v-list-item
+          link
+          prepend-icon="mdi-home"
+          title="Home"
+          @click="navigateTo('/')"
+        />
+        <v-list-item
+          link
+          prepend-icon="mdi-plus"
+          title="Add Transaction"
+          @click="navigateTo('/new-transaction')"
+        />
+
+        <v-divider class="mt-auto"/>
+        <v-list-item>
+          <v-list-item-icon v-if="rail">
+            <v-btn
+              :icon="themeVal === 'light' ? 'mdi-weather-night' : 'mdi-weather-sunny'"
+              rounded="0"
+              block
+              @click="onThemeButtonClick"
+            />
+          </v-list-item-icon>
+          <v-switch
+            v-if="!rail"
+            hide-details
+            v-model="themeVal"
+            true-value="light"
+            false-value="dark"
+            true-icon="mdi-weather-night"
+            false-icon="mdi-weather-sunny"
+            :color="themeVal === 'dark' ? 'grey-lighten-5' : 'grey-darken-4'"
+            inset
+            @update:model-value="toggleTheme"
+          />
+        </v-list-item>
+        
+        <v-list-item
+          :prepend-avatar="user.profilePicture ?? undefined"
+          :title="user.name ?? undefined"
+          :subtitle="user.email ?? undefined"
+        >
+          <template v-slot:append>
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  variant="text"
+                  icon="mdi-dots-vertical"
+                  v-bind="props"
+                ></v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="menuItemClicked('Profile')">
+                  <v-list-item-icon>
+                    <v-icon>mdi-account</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Profile</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click="menuItemClicked('Settings')">
+                  <v-list-item-icon>
+                    <v-icon>mdi-cog</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Settings</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item @click="menuItemClicked('Logout')">
+                  <v-list-item-icon>
+                    <v-icon>mdi-logout</v-icon>
+                  </v-list-item-icon>
+                  <v-list-item-content>
+                    <v-list-item-title>Logout</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </template>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar>
-      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
-
       <v-app-bar-title>Application</v-app-bar-title>
     </v-app-bar>
 
@@ -74,18 +109,25 @@ import { useTheme } from 'vuetify'
 const theme = useTheme();
 const user = useAuthenticatedUser();
 
-const drawer = ref(false);
 const themeVal  = ref(theme.global.name.value);
-const menu = ref(false);
-const position = ref({ x: 0, y: 0 });
+const rail = ref(false);
 
-const toggleTheme = () => theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark';
-const toggleDrawer = () => drawer.value = !drawer.value;
-const toggleMenu = (e: MouseEvent) => {
-  menu.value = !menu.value;
-  position.value.x = e.clientX;
-  position.value.y = e.clientY;
+
+const toggleTheme = () => {
+  console.log('onThemeToggle', themeVal.value);
+  theme.global.name.value = themeVal.value;
 };
+
+const onThemeButtonClick = () => {
+  themeVal.value = themeVal.value === 'light' ? 'dark' : 'light';
+  toggleTheme();
+};
+
+const toggleRail = () => {
+  console.log('onRailToggle');
+  rail.value = !rail.value;
+};
+
 
 const menuItemClicked = async (item: string) => {
   switch (item) {
